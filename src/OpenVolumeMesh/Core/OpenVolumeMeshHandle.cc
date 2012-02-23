b5e8bef8be5b8be2afb1e72356cc596062222954
Mike Kremer <mkremer@cs.rwth-aachen.de>
2012-02-23 16:31:49 +0000

    Added streaming operator for OpenVolumeMesh handles.
    git-svn-id: http://www.openvolumemesh.org/svnrepo/OpenVolumeMesh/trunk@113 66977474-1d4b-4f09-8fe9-267525286df2

--- src/OpenVolumeMesh/Core/OpenVolumeMeshHandle.cc
+++ src/OpenVolumeMesh/Core/OpenVolumeMeshHandle.cc
@@ -34,64 +34,26 @@
 
 /*===========================================================================*\
  *                                                                           *
- *   $Revision$                                                         *
- *   $Date$                    *
- *   $LastChangedBy$                                                *
+ *   $Revision: 36 $                                                         *
+ *   $Date: 2012-01-10 18:00:06 +0100 (Di, 10 Jan 2012) $                    *
+ *   $LastChangedBy: kremer $                                                *
  *                                                                           *
 \*===========================================================================*/
 
-#ifndef OPENVOLUMEMESHHANDLE_HH_
-#define OPENVOLUMEMESHHANDLE_HH_
+#include "OpenVolumeMeshHandle.hh"
 
 namespace OpenVolumeMesh {
 
-// Define handle types in order to distinguish different entities by their indices
-class OpenVolumeMeshHandle {
-public:
-    // Default constructor
-	explicit OpenVolumeMeshHandle(int _idx) : idx_(_idx) {};
+std::ostream& operator<<(std::ostream& _ostr, const OpenVolumeMeshHandle& _handle) {
+    _ostr << _handle.idx();
+    return _ostr;
+}
 
-	OpenVolumeMeshHandle& operator=(int _idx) {
-		idx_ = _idx;
-		return *this;
-	}
-
-	OpenVolumeMeshHandle& operator=(const OpenVolumeMeshHandle& _idx) {
-		idx_ = _idx.idx_;
-		return *this;
-	}
-
-	inline bool is_valid() const { return idx_ != -1; }
-
-	inline bool operator<(const OpenVolumeMeshHandle& _idx) const { return (this->idx_ < _idx.idx_); }
-
-	inline bool operator<(int _idx) const { return idx_ < _idx; }
-
-	inline int idx() const { return idx_; }
-
-	void idx(const int& _idx) { idx_ = _idx; }
-
-	void reset() { idx_ = -1; }
-
-	operator int() const { return idx_; }
-
-private:
-	int idx_;
-};
-
-// Default entity handles
-
-class VertexHandle   : public OpenVolumeMeshHandle { public: VertexHandle(int _idx = -1)   : OpenVolumeMeshHandle(_idx) {} };
-class EdgeHandle     : public OpenVolumeMeshHandle { public: EdgeHandle(int _idx = -1)     : OpenVolumeMeshHandle(_idx) {} };
-class FaceHandle     : public OpenVolumeMeshHandle { public: FaceHandle(int _idx = -1)     : OpenVolumeMeshHandle(_idx) {} };
-class CellHandle     : public OpenVolumeMeshHandle { public: CellHandle(int _idx = -1)     : OpenVolumeMeshHandle(_idx) {} };
-class HalfEdgeHandle : public OpenVolumeMeshHandle { public: HalfEdgeHandle(int _idx = -1) : OpenVolumeMeshHandle(_idx) {} };
-class HalfFaceHandle : public OpenVolumeMeshHandle { public: HalfFaceHandle(int _idx = -1) : OpenVolumeMeshHandle(_idx) {} };
-
-std::ostream& operator<<(std::ostream& _ostr, const OpenVolumeMeshHandle& _handle);
-
-std::istream& operator>>(std::istream& _istr, OpenVolumeMeshHandle& _handle);
+std::istream& operator>>(std::istream& _istr, OpenVolumeMeshHandle& _handle) {
+    int val = 0;
+    _istr >> val;
+    _handle.idx(val);
+    return _istr;
+}
 
 } // Namespace OpenVolumeMesh
-
-#endif /* OPENVOLUMEMESHHANDLE_HH_ */
