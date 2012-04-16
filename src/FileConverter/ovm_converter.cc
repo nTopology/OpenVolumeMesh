0dd0d4354ab7557d6a6c8ec8c8cbfa7582a20e2a
Mike Kremer <mkremer@cs.rwth-aachen.de>
2012-04-16 12:46:29 +0000

    Added grammar for netgen files
    git-svn-id: http://www.openvolumemesh.org/svnrepo/OpenVolumeMesh/trunk@160 66977474-1d4b-4f09-8fe9-267525286df2

--- src/FileConverter/ovm_converter.cc
+++ src/FileConverter/ovm_converter.cc
@@ -6,17 +6,24 @@
 #include <OpenVolumeMesh/FileManager/FileManager.hh>
 
 #include "MeshGenerator.hpp"
+
 #include "Grammars/Tetmesh.hpp"
+#include "Grammars/Netgen.hpp"
+
+void printUsage() {
+    std::cerr << "You need to specify a format and a source file to convert!" << std::endl << std::endl;
+    std::clog << "Usage: file_converter <format> <filename> [output_filename.ovm]" << std::endl << std::endl;
+    std::clog << "Available file formats:" << std::endl;
+    std::clog << "  -t\tTetmesh" << std::endl;
+    std::clog << "  -n\tNetgen" << std::endl;
+    std::clog << std::endl;
+}
 
 int main(int _argc, char* _argv[]) {
 
     if(_argc < 3 || _argc > 4 ||
             (_argc > 1 && (std::strcmp(_argv[1], "--help") == 0 || std::strcmp(_argv[1], "-h") == 0))) {
-        std::cerr << "You need to specify a source file to convert!" << std::endl << std::endl;
-        std::clog << "Usage: file_converter <format> <filename> [output_filename]" << std::endl << std::endl;
-        std::clog << "Available file formats:" << std::endl;
-        std::clog << "  -t\tTetmesh" << std::endl;
-        std::clog << std::endl;
+        printUsage();
         return -1;
     }
 
@@ -36,13 +43,24 @@ int main(int _argc, char* _argv[]) {
     std::string fileContent = oss.str();
 
     // Instantiate grammar objects
-    tetmesh_grammar<std::string::iterator> grammar(generator);
+    tetmesh_grammar<std::string::iterator> tetmesh_grammar(generator);
+    netgen_grammar<std::string::iterator> netgen_grammar(generator);
 
     bool r = false;
 
     if(std::strcmp(_argv[1], "-t") == 0) {
+
         // Tetmesh format
-        r = boost::spirit::qi::phrase_parse(fileContent.begin(), fileContent.end(), grammar, qi::space);
+        r = boost::spirit::qi::phrase_parse(fileContent.begin(), fileContent.end(), tetmesh_grammar, qi::space);
+
+    } else if(std::strcmp(_argv[1], "-n") == 0) {
+
+        // Netgen format
+        r = boost::spirit::qi::phrase_parse(fileContent.begin(), fileContent.end(), netgen_grammar, qi::space);
+
+    } else {
+        printUsage();
+        return -1;
     }
 
     if(r) {
