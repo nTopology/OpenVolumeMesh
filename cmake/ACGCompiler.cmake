60e92894f8bff376d571d64dd32dbfe1f0718cd4
Jan Möbius <moebius@cs.rwth-aachen.de>
2014-06-11 11:04:34 +0000

    Removed the c++11 part. does not seem to work correctly
    git-svn-id: http://www.openvolumemesh.org/svnrepo/OpenVolumeMesh/trunk@274 66977474-1d4b-4f09-8fe9-267525286df2

--- cmake/ACGCompiler.cmake
+++ cmake/ACGCompiler.cmake
@@ -153,25 +153,25 @@ if (UNIX)
   ################################################################################
 
   # On apple, if we have c++ 11 support, we enable it automatically here
-  if (APPLE)
-
-    include(CheckCXXCompilerFlag)
-    CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
-    CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
-
-    if(COMPILER_SUPPORTS_CXX11)
-       set( CMAKE_CXX_FLAGS_DEBUG          "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11")
-       set( CMAKE_CXX_FLAGS_RELEASE        "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11")
-       set( CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11")
-    elseif(COMPILER_SUPPORTS_CXX0X)
-       set( CMAKE_CXX_FLAGS_DEBUG          "${CMAKE_CXX_FLAGS_DEBUG} -std=c++0x")
-       set( CMAKE_CXX_FLAGS_RELEASE        "${CMAKE_CXX_FLAGS_DEBUG} -std=c++0x")
-       set( CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_DEBUG} -std=c++0x")
-    else()
-        message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Falling back to non C++11 mode. If you encounter errors, please use a different C++ compiler.")
-    endif()
-
-  endif()
+#  if (APPLE)
+#
+#    include(CheckCXXCompilerFlag)
+#    CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+#    CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+#
+#    if(COMPILER_SUPPORTS_CXX11)
+#       set( CMAKE_CXX_FLAGS_DEBUG          "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11")
+#       set( CMAKE_CXX_FLAGS_RELEASE        "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11")
+#       set( CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_DEBUG} -std=c++11")
+#    elseif(COMPILER_SUPPORTS_CXX0X)
+#       set( CMAKE_CXX_FLAGS_DEBUG          "${CMAKE_CXX_FLAGS_DEBUG} -std=c++0x")
+#       set( CMAKE_CXX_FLAGS_RELEASE        "${CMAKE_CXX_FLAGS_DEBUG} -std=c++0x")
+#       set( CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_DEBUG} -std=c++0x")
+#    else()
+#        message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Falling back to non C++11 mode. If you encounter errors, please use a different C++ compiler.")
+#    endif()
+#
+#  endif()
 
   
 endif ()
