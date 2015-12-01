e2bfb633721f0b953a37b122db7862356fd32bdd
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-12-01 09:03:37 +0100

    Added ci jobs for OpenVolumemesh

---
+++ CI/ci-mac.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#Exit on any error
+set -e
+
+LANGUAGE=$1
+
+PATH=$PATH:/opt/local/bin
+export PATH
+
+OPTIONS=""
+
+if [ "$LANGUAGE" == "C++98" ]; then
+  echo "Building with C++98";
+elif [ "$LANGUAGE" == "C++11" ]; then
+  echo "Building with C++11";
+  OPTIONS="$OPTIONS -DCMAKE_CXX_FLAGS='-std=c++11' "
+fi
+
+
+#########################################
+# Build release version
+#########################################
+
+if [ ! -d build-release ]; then
+  mkdir build-release
+fi
+
+cd build-release
+
+cmake -DCMAKE_BUILD_TYPE=Release -DSTL_VECTOR_CHECKS=ON $OPTIONS ../
+
+#build it
+make
+
+#build the unit tests
+make unittests
+
+
+#########################################
+# Run Release Unittests
+#########################################
+cd Unittests
+
+#execute tests
+./unittests --gtest_color=yes --gtest_output=xml
+
+cd ..
+cd ..
+
+
+#########################################
+# Build Debug version and Unittests
+#########################################
+
+if [ ! -d build-debug ]; then
+  mkdir build-debug
+fi
+
+cd build-debug
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DSTL_VECTOR_CHECKS=ON $OPTIONS ../
+
+#build it
+make
+
+#build the unit tests
+make unittests
+
+
+#########################################
+# Run Debug Unittests
+#########################################
+
+cd Unittests
+
+# Run the unittests
+./unittests --gtest_color=yes --gtest_output=xml
