e06b982cfb8284cc63c6fd7f6dcc65821c158273
Jan Möbius <moebius@cs.rwth-aachen.de>
2015-12-01 13:28:02 +0100

    Split unittests into seperate files

--- src/Unittests/unittests.cc
+++ src/Unittests/unittests.cc
@@ -1,11 +1,5 @@
 #include <gtest/gtest.h>
 
-#include "unittests_common.hh"
-#include "unittests_basics.hh"
-#include "unittests_iterators.hh"
-#include "unittests_properties.hh"
-#include "unittests_files.hh"
-
 int main(int _argc, char** _argv) {
 
     testing::InitGoogleTest(&_argc, _argv);
