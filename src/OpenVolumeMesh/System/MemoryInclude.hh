7176e9ff0039cb3369681ce84ae44fadba55f4f1
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-11-10 09:14:13 +0100

    Fixed license header

--- src/OpenVolumeMesh/System/MemoryInclude.hh
+++ src/OpenVolumeMesh/System/MemoryInclude.hh
@@ -1,54 +1,47 @@
 /*===========================================================================*\
  *                                                                           *
- *                              OpenFlipper                                  *
- *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
- *           Department of Computer Graphics and Multimedia                  *
- *                          All rights reserved.                             *
- *                            www.openflipper.org                            *
+ *                            OpenVolumeMesh                                 *
+ *        Copyright (C) 2011 by Computer Graphics Group, RWTH Aachen         *
+ *                        www.openvolumemesh.org                             *
  *                                                                           *
  *---------------------------------------------------------------------------*
- * This file is part of OpenFlipper.                                         *
- *---------------------------------------------------------------------------*
+ *  This file is part of OpenVolumeMesh.                                     *
  *                                                                           *
- * Redistribution and use in source and binary forms, with or without        *
- * modification, are permitted provided that the following conditions        *
- * are met:                                                                  *
+ *  OpenVolumeMesh is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
  *                                                                           *
- * 1. Redistributions of source code must retain the above copyright notice, *
- *    this list of conditions and the following disclaimer.                  *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
  *                                                                           *
- * 2. Redistributions in binary form must reproduce the above copyright      *
- *    notice, this list of conditions and the following disclaimer in the    *
- *    documentation and/or other materials provided with the distribution.   *
+ *  OpenVolumeMesh is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
  *                                                                           *
- * 3. Neither the name of the copyright holder nor the names of its          *
- *    contributors may be used to endorse or promote products derived from   *
- *    this software without specific prior written permission.               *
- *                                                                           *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       *
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED *
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A           *
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER *
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  *
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       *
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        *
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    *
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      *
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        *
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with OpenVolumeMesh.  If not,                              *
+ *  see <http://www.gnu.org/licenses/>.                                      *
  *                                                                           *
 \*===========================================================================*/
 
 #ifndef ACG_UTILS_SMARTPOINTER_HH
 #define ACG_UTILS_SMARTPOINTER_HH
 
-/*************************************************
- * Warning! This header file is duplicated in    *
- * OpenVolumeMesh with the same header guard,    *
- * as src/OpenVolumeMesh/System/MemoryInclude.hh.*
- * If you change this file, you should change    *
- * that file as well.                            *
- *************************************************/
+ /**********************************************
+  * Warning! This header file is duplicated in *
+  * OpenFlipper with the same header guard, as *
+  * ACG/Utils/SmartPointer.hh.                 *
+  * If you change this file, you should change *
+  * that file as well.                         *
+  **********************************************/
 
 #include <memory>
 
